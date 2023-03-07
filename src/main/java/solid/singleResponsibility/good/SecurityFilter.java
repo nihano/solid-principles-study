a3ab8ee843443ceea82d3b6080b6e3d7d7935f13
nihano <nioztekin@gmail.com>
2023-03-08 00:36:43 +0300

    SecurityFilter

---
+++ src/main/java/solid/singleResponsibility/good/SecurityFilter.java
@@ -0,0 +1,14 @@
+package solid.singleResponsibility.good;
+
+public class SecurityFilter {
+    public static boolean authenticate() {
+        // Check authentication
+        return true;
+    }
+
+    public static boolean authorization() {
+        // Check authorization
+        return true;
+    }
+
+}
